27ae840a093200ed782a85418cbe926f791007be
Agustin Soria <soriaagustin8@gmail.com>
2023-10-10 05:57:03 -0300

    fixing linter errors

--- student.rb
+++ student.rb
@@ -1,10 +1,10 @@
 class Student < Person
-    def initialize(id, age:, classroom:, name: 'Unknown', parent_permission: true)
-        super(id, age: age, name: name, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+  def initialize(id, age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(id, age: age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hookey
-        "¯\(ツ)/¯"
-    end
+  def play_hookey
+    '¯(ツ)/¯'
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,10 @@
 class Teacher < Person
-    def initialize(id, age, specialization, name: 'Unknown', parent_permission: true)
-        super(id, name: name, age: age, parent_permission: parent_permission)
-        @specialization = specialization
-    end
+  def initialize(id, age, specialization, name: 'Unknown', parent_permission: true)
+    super(id, name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_service?
-        true
-    end
+  def can_use_service?
+    true
+  end
 end
