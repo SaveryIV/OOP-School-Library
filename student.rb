33eeef8ca82278cf9f4db3179d41220c0817d429
Agustin Soria <soriaagustin8@gmail.com>
2023-10-13 05:27:13 -0300

    fixing linters2

--- student.rb
+++ student.rb
@@ -1,17 +1,21 @@
+require_relative 'person'
+
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(id, age:, classroom:, name: 'Unknown', parent_permission: true)
     super(id, age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def classroom=(classroom)
+    @classroom&.students&.delete(self)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
-  def play_hookey
-    '¯(ツ)/¯'
+  def play_hooky
+    '¯\\(ツ)/¯'
   end
 end
