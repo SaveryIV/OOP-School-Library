51b62b4c1297c15ac461077cae4ccd6326d3ef70
Agustin Soria <soriaagustin8@gmail.com>
2023-10-13 18:13:23 -0300

    Update files to work in the terminal

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,6 @@
 class Teacher < Person
-  def initialize(id, age, specialization, name: 'Unknown', parent_permission: true)
-    super(id, name: name, age: age, parent_permission: parent_permission)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
