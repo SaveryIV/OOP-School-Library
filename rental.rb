40f180f9f85af4aa970488cb3f17a03537b537b9
Agustin Soria <soriaagustin8@gmail.com>
2023-10-14 00:24:26 -0300

    Update rental file

--- rental.rb
+++ rental.rb
@@ -1,17 +1,11 @@
 class Rental
-  attr_accessor :date, :books, :person
+  attr_accessor :date, :book, :person
 
   def initialize(date, books, person)
     @date = date
-    @books = []
     @person = person
-
-    books.each { |book| add_book(book) }
-    person.add_rental(self)
-  end
-
-  def add_book(book)
-    @books << book
-    book.add_rental(self)
+    @book = books
+    person.rentals << self
+    books.rentals << self
   end
 end
